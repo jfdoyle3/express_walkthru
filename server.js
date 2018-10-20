585d2e3c18346ed9bbd0f9ee424c5fabd39f39c3
Jim Doyle <indellibleink@gmail.com>
2018-10-20 12:09:59 -0400

    Updated file with walkthru

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express=require('express');
-const app=express();
+con.st app=express();
 
-app.get('/', (request,response)=> response.send('hello'));
+app.get('/', (request,response)=> response.send('<h1>hello</h1>'));
+
+app.get ('/test', (res,req)=> res.send('<h1>test route</h1>'))
+
+app.get('/test/:num',(req,res)=> res.send(req.params.num))
 
 
+const port= process.env.PORT || 5000 
 
-
-const port= process.env.PORT || 5000
-
-app.listen(port, ()=> console.log("port open "+port));
+app.listen(port, ()=> console.log( 'App listening on port ${port}'))
