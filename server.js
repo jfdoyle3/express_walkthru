dcc087819a3f3c91f8c10b785ce02ce8c866feef
Jim Doyle <indellibleink@gmail.com>
2018-10-20 12:21:22 -0400

    fixed typos

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ app.get('/', (request,response)=> response.send('<h1>hello</h1>'));
 
 app.get ('/test', (res,req)=> res.send('<h1>test route</h1>'))
 
-app.get('/test/:num',(req,res)=> res.send(req.params.num))
+app.get('/test/:num',(req,res)=> res.send(`<h1>${req.params.num}</h1>`))
+
+app.get('/test/add/:num',(req,res)=> {
+    const num=parseInt(req.params.num,10)
+    res.send(`<h1>${5+num}</h1>`)
+
+})
+
+
+
 
 
 const port= process.env.PORT || 5000 
 
-app.listen(port, ()=> console.log( 'App listening on port ${port}'))
+app.listen(port, ()=> console.log( `App listening on port ${port}`))
